d995869b1868db92f6c518be7f35c4faa1d32722
d3enis <79760898+d3enis@users.noreply.github.com>
2022-05-29 21:46:12 +0200

    Added country prefix, date and rounded temp value

--- src/Widget.js
+++ src/Widget.js
@@ -12,11 +12,52 @@ const Widget = () => {
     });
   }, []);
 
-  let { name, main: { temp } = "", wind: { speed } = "" } = result;
+  const dateGenerator = (date) => {
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let number = date.getDate();
+    let day = days[date.getDay()];
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+
+    return `${day}  ${number} ${month} ${year} `;
+  };
+
+  let {
+    name,
+    sys: { country } = "",
+    main: { temp } = "",
+    wind: { speed } = "",
+  } = result;
   return (
-    <div className="widget">
-      <h1>{name}</h1>
-      <p>Temp: {temp}℃</p>
+    <div className="widget warm-bg">
+      <h1>
+        {name},{country}
+      </h1>
+      <p>{dateGenerator(new Date())}</p>
+      <p>Temp: {Math.round(temp)}℃</p>
       <p>Wind speed : {speed} km/h</p>
     </div>
   );
